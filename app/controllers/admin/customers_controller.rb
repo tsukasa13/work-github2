7c020c5c99d28e594bfe1cb9911232b2f5d8f061
EC2 Default User <ec2-user@ip-172-31-44-51.ap-northeast-1.compute.internal>
2024-05-04 16:11:37 +0000

    [Add] 管理者の会員一覧

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,15 +1,16 @@
 class Admin::CustomersController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def index
+    @customers = Customer.all
   end
-  
+
   def show
   end
-  
+
   def edit
   end
-  
+
   def update
   end
 end
