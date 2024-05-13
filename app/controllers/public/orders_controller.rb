d3f4259bfb49c295eb31e6cbef517ef0ea7d1967
EC2 Default User <ec2-user@ip-172-31-44-51.ap-northeast-1.compute.internal>
2024-05-13 06:53:17 +0000

    [Add] 注文情報入力

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -8,6 +8,11 @@ class Public::OrdersController < ApplicationController
 
   def confirm
     @order = Order.new(order_params)
+    @order.shipping_postal_code = current_customer.postal_code
+    @order.shipping_address = current_customer.address
+    @order.delivery_name = current_customer.last_name + current_customer.first_name
+    @cart_items = current_customer.cart_items.all
+    @order.customer_id = current_customer.id
   end
 
   def complete
