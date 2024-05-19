a03aa6f88cd3da790648815867cbf0fe414591c4
EC2 Default User <ec2-user@ip-172-31-44-51.ap-northeast-1.compute.internal>
2024-05-19 14:30:27 +0000

    [Add] 管理者の注文履歴一覧

--- config/routes.rb
+++ config/routes.rb
@@ -37,5 +37,6 @@ Rails.application.routes.draw do
     root 'homes#top'
     resources :items, only: [:new, :create, :index, :show, :edit, :update]
     resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show]
   end
 end
