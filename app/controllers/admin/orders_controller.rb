3a84f82ce50be0880c194ce77f500470a2e16f47
EC2 Default User <ec2-user@ip-172-31-44-51.ap-northeast-1.compute.internal>
2024-05-22 08:48:35 +0000

    [Add] 管理者ページネーション

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,6 +3,6 @@ class Admin::OrdersController < ApplicationController
 
   def show
     @order = Order.find(params[:id])
-    @order_details = OrderDetail.all
+    @order_details = OrderDetail.page(params[:page])
   end
 end
